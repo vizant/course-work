561c33ee8b95e0f0dd6328a424439000cc38c0be
viuk0321 <viktor.ukolov@netcracker.com>
2021-11-14 15:01:28 +0300

    changed encoding

---
+++ src/main/java/com/ssau/pmi/utils/Constants.java
@@ -0,0 +1,17 @@
+package com.ssau.pmi.utils;
+
+public class Constants {
+
+    public static final String CHMMF_COURSE = "\u0427\u041C\u041C\u0424 \u043A\u0443\u0440\u0441\u043E\u0432\u0430\u044F";
+    public static final String VALUE = "\u0417\u043D\u0430\u0447\u0435\u043D\u0438\u0435";
+    public static final String SPACE = " ";
+    public static final String EQUAL = "=";
+    public static final String PARAMETERS_INFO_CN = "\u0413\u0440\u0430\u0444\u0438\u043A \u043F\u0440\u0438 "
+            + " I = %d, J = %d " + "\u0438 \u0444\u0438\u043A\u0441\u0438\u0440\u043E\u0432\u0430\u043D\u043D\u043E\u043C "
+            + "%s \u0434\u043B\u044F \u0441\u0445\u0435\u043C\u044B %s";
+    public static final String PARAMETERS_INFO_IMPLICIT = "\u0413\u0440\u0430\u0444\u0438\u043A \u043F\u0440\u0438 "
+            + " I = %d, J = %d " + "\u0438 \u0444\u0438\u043A\u0441\u0438\u0440\u043E\u0432\u0430\u043D\u043D\u043E\u043C "
+            + "%s \u0434\u043B\u044F %s \u0441\u0445\u0435\u043C\u044B";
+    public static final String FOR_CN = "\u041A\u0440\u0430\u043D\u043A\u0430-\u041D\u0438\u043A\u043E\u043B\u0441\u043E\u043D\u0430";
+    public static final String FOR_IMPLICIT = "\u043D\u0435\u044F\u0432\u043D\u043E\u0439";
+}
